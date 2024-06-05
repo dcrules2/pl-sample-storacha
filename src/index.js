50328beb42a8954c415abb42d9eef93b039db410
dcrules2 <169724415+dcrules2@users.noreply.github.com>
2024-06-05 13:03:14 -0500

    feat: html function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const { URL } = require('url');
 import * as W3UPClient from '@web3-storage/w3up-client';
+
+//Access HTML source code
+const url = https://http.cat/
+
+function getHTML(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => resolve(data));
+      }).on('error', (err) => reject(err));
+    });
+  }
